afb0e08d6f2ed3e369d51179acdaf809fd6d78c9
jaehoon <icelift@naver.com>
2024-09-06 00:23:06 +0900

    2024.09.05 목
    시큐리티 활성화 로그인 수정중

--- src/main/java/com/project/myapp/login/controller/LoginController.java
+++ src/main/java/com/project/myapp/login/controller/LoginController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,6 +31,7 @@ public class LoginController {
 	private final Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
 	private final ClientRegistrationRepository clientRegistrationRepository;
 	private final OAuth2Properties oAuth2Properties;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@GetMapping("/logout")
 	public String logOut(HttpSession session, HttpServletRequest request) {
--- src/main/java/com/project/myapp/utiles/UserValidator.java
+++ src/main/java/com/project/myapp/utiles/UserValidator.java
@@ -8,15 +8,8 @@ import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import com.project.myapp.dto.UserDTO;
-import com.project.myapp.utiles.properties.ErrorMesageProperties;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class UserValidator implements Validator {
-
-	public ErrorMesageProperties errorMesageProperties;
-
 	@Override
 	public boolean supports(Class<?> clazz) {
 		return UserDTO.class.isAssignableFrom(clazz);
@@ -24,7 +17,7 @@ public class UserValidator implements Validator {
 
 	@Override
 	public void validate(Object target, Errors errors) {
-		log.info("target = {}", target);
+		System.out.println("target = " + target);
 
 		UserDTO userDTO = (UserDTO)target;
 
@@ -33,15 +26,15 @@ public class UserValidator implements Validator {
 		String email = userDTO.getEmail();
 		String nickName = userDTO.getNickName();
 
-		String idPattern = "^[a-zA-Z0-9]+$";
-		String passwordPattern = "^(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%^&*(),.?\":{}|<>]+$";
-		String emailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
-		String nickNamePattern = "^(?=.*[가-힣])?(?=.*[a-zA-Z])?(?=.*\\d)?[가-힣a-zA-Z\\d]+$";
-
-		// String idPattern = "^[a-zA-Z0-9]$";
+		// String idPattern = "^[a-zA-Z0-9]+$";
 		// String passwordPattern = "^(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%^&*(),.?\":{}|<>]$";
 		// String emailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 		// String nickNamePattern = "^(?=.*[가-힣])?(?=.*[a-zA-Z])?(?=.*\\d)?[가-힣a-zA-Z\\d]$";
+
+		String idPattern = "^[a-zA-Z0-9]{8,12}$";
+		String passwordPattern = "^(?=.*[a-zA-Z])[a-zA-Z0-9!@#$%^&*(),.?\":{}|<>]{8,20}$";
+		String emailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+		String nickNamePattern = "^(?=.*[가-힣])?(?=.*[a-zA-Z])?(?=.*\\d)?[가-힣a-zA-Z\\d]{3,8}$";
 
 		Pattern pattern = Pattern.compile(idPattern, Pattern.CASE_INSENSITIVE);
 		Pattern pattern2 = Pattern.compile(passwordPattern, Pattern.CASE_INSENSITIVE);
@@ -53,28 +46,23 @@ public class UserValidator implements Validator {
 		Matcher matcherEmail = pattern3.matcher(email);
 		Matcher matcherNickname = pattern4.matcher(nickName);
 
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "id", "required", errorMesageProperties.getErrorId());
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "required",
-			errorMesageProperties.getErrorPassword());
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nickName", "required",
-			errorMesageProperties.getErrorNickname());
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "required", errorMesageProperties.getErrorEmail());
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "id", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nickName", "required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "required");
 
 		if (id.length() < 8 || id.length() > 12) {
-			errors.rejectValue("id", "invalidLength", new String[] {"8", "12"},
-				errorMesageProperties.getErrorInvalidLengthNickname());
+			errors.rejectValue("id", "invalidLength", new String[] {"8", "12"}, null);
 		}
 		if (password.length() < 8 || password.length() > 20) {
-			errors.rejectValue("password", "invalidLength", new String[] {"8", "20"},
-				errorMesageProperties.getErrorInvalidLengthPassword());
+			errors.rejectValue("password", "invalidLength", new String[] {"8", "20"}, null);
 		}
 		if (nickName.length() < 3 || nickName.length() > 8) {
-			errors.rejectValue("nickName", "invalidLength", new String[] {"3", "8"},
-				errorMesageProperties.getErrorInvalidLengthNickname());
+			errors.rejectValue("nickName", "invalidLength", new String[] {"3", "8"}, null);
 		}
 
 		if (!matcherId.matches()) {
-			errors.rejectValue("id", "invalidId");
+			errors.rejectValue("id", "invalidId", null);
 		}
 		if (!matcherPassword.matches()) {
 			errors.rejectValue("password", "invalidPassword", null);
@@ -85,5 +73,6 @@ public class UserValidator implements Validator {
 		if (!matcherNickname.matches()) {
 			errors.rejectValue("nickName", "invalidNickName", null);
 		}
+
 	}
 }
