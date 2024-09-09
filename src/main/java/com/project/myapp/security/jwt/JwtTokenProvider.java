46714fe90e98c2bac96bbf5df73bc21b79881beb
jaehoon <icelift@naver.com>
2024-09-10 00:28:14 +0900

    2024.09.09 월
    RedisConfig 연결설정코드 주석추가

--- src/main/java/com/project/myapp/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/project/myapp/security/jwt/JwtTokenProvider.java
@@ -1,77 +1,79 @@
 package com.project.myapp.security.jwt;
 
+import java.util.Base64;
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
 import com.project.myapp.security.auth.CustomDetails;
 import com.project.myapp.security.auth.CustomDetailsService;
 import com.project.myapp.utiles.properties.JwtProperties;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import javax.servlet.http.HttpServletRequest;
-import java.util.Base64;
-import java.util.Date;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtTokenProvider {
-    private final CustomDetailsService customDetailsService;
-    private final JwtProperties jwtProperties;
-    @Value("${jwt.token.key}")
-    private String secretKey;
+	private final CustomDetailsService customDetailsService;
+	private final JwtProperties jwtProperties;
+	private String secretKey;
 
-    private Long tokenValidTile = 240 * 60 * 1000L;
+	private Long tokenValidTile = 240 * 60 * 1000L;
 
-    @PostConstruct
-    public void init() {
-        secretKey = Base64.getEncoder().encodeToString(jwtProperties.getJwtTokenKey().getBytes());
-    }
+	@PostConstruct
+	public void init() {
+		secretKey = Base64.getEncoder().encodeToString(jwtProperties.getJwtTokenKey().getBytes());
+	}
 
-    // JWT토큰 생성
-    public String createToken(String email , String role) {
-        Date now = new Date();
-        
-        Claims claims = Jwts.claims()
-                .setSubject("access_token")
-                .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + tokenValidTile));
+	// JWT토큰 생성
+	public String createToken(String email, String role) {
+		Date now = new Date();
 
-        claims.put("email", email);
-        claims.put("role", role);
-        return Jwts.builder()
-                .setHeaderParam("typ", "JWT")
-                .setClaims(claims)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
-                .compact();
-    }
-    
-    public String resolveToken(HttpServletRequest request) {
-        return request.getHeader("JWT");
-    }
+		Claims claims = Jwts.claims()
+			.setSubject("access_token")
+			.setIssuedAt(now)
+			.setExpiration(new Date(now.getTime() + tokenValidTile));
 
-    public boolean validateToken(String jwtToken) {
+		claims.put("email", email);
+		claims.put("role", role);
+		return Jwts.builder()
+			.setHeaderParam("typ", "JWT")
+			.setClaims(claims)
+			.signWith(SignatureAlgorithm.HS256, secretKey)
+			.compact();
+	}
 
-        try {
-            Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken).getBody();
-            return !claims.getExpiration().before(new Date());
-        } catch (Exception e) {
-            return false;
-        }
-    }
+	public String resolveToken(HttpServletRequest request) {
+		return request.getHeader("JWT");
+	}
 
-    public Authentication getAuthentication(String token) {
+	public boolean validateToken(String jwtToken) {
 
-        CustomDetails customDetails = (CustomDetails) customDetailsService.loadUserByUsername(this.getUserPk(token));
-        return new UsernamePasswordAuthenticationToken(customDetails,"",customDetails.getAuthorities());
-    }
-    public String getUserPk(String token){
-        return (String) Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().get("email");
-    }
+		try {
+			Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken).getBody();
+			return !claims.getExpiration().before(new Date());
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public Authentication getAuthentication(String token) {
+
+		CustomDetails customDetails = (CustomDetails)customDetailsService.loadUserByUsername(this.getUserPk(token));
+		return new UsernamePasswordAuthenticationToken(customDetails, "", customDetails.getAuthorities());
+	}
+
+	public String getUserPk(String token) {
+		return (String)Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().get("email");
+	}
 }
--- src/main/java/com/project/myapp/security/jwt/RedisConfig.java
+++ src/main/java/com/project/myapp/security/jwt/RedisConfig.java
@@ -25,11 +25,25 @@ public class RedisConfig {
 
 	private final JwtProperties jwtProperties;
 
+	/**
+	 * Redis 서버와 연결을 생성하고 관리하는 역할
+	 * Spring ↔ Redis 통신을 위해 사용하는 인터페이스
+	 * @return
+	 */
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
 		return new LettuceConnectionFactory(jwtProperties.getHost(), jwtProperties.getPort());
 	}
 
+	/**
+	 * RedisTemplate : Redis에 데이터를 읽고 쓰기 위한 주요 템플릿 클래스
+	 * ConnectionFactory 설정 : 위에서 생성한 redisConnectionFactory를 사용하여 Redis와의 연결 설정
+	 * key-value 직렬화 설정 : 키-해시키, 해시 값 모두를 StringRedisSerializer로 직렬화 하여 Redis에 저장하거나 읽어올때 문자열 형식으로 처리
+	 * → Redis와의 데이터 직렬화 및 역직렬화 관리
+	 * Redis에 key-value 저장소 작업을 수행할 수 있도록 하는 인터페이스로 사용
+	 * @param redisConnectionFactory 위에서 생성된 연결객체
+	 * @return
+	 */
 	@Bean
 	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
 		RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
@@ -40,6 +54,15 @@ public class RedisConfig {
 		return redisTemplate;
 	}
 
+	/**
+	 * Spring의 캐시추상화를 사용하여 Redis를 캐시 스토어로 사용하는 캐시 관리자를 제공
+	 * RedisCacheManagerBuilder : redisConnectionFactory를 사용하여 Redis와의 연결 설정
+	 * RedisCacheConfiguration : 기본 캐시 설정 지정
+	 * - 값 직렬화 : GenericJackson2JsonRedisSerializer를 사용하여 캐시된 데이터를 JSON 형식으로 직렬화
+	 * TTL(Time to Live) : 캐시항목의 기본 TTL을 설정(여기선 5분 설정)
+	 * @param redisConnectionFactory 위에서 생성된 연결 객체
+	 * @return
+	 */
 	@Bean
 	public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
 		RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager.builder(redisConnectionFactory);
